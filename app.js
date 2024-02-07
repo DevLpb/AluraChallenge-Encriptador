c54f248ef85ea389a05fd03ce5a8452d8b3705d6
Leonel <leonelpiedrabuena04@gmail.com>
2024-02-07 17:07:30 -0300

    Avances en estilos y lógica
    Cambios realizados en elementos, nombres de clases, nuevos estilos y primeras funciones

---
+++ app.js
@@ -0,0 +1,31 @@
+function encriptar() {
+    document.getElementById("figura").style.display = "none";
+    document.getElementById("mensaje").style.display = "none";
+    document.getElementById("texto-salida").style.display = "block";
+}
+
+
+
+/* Posibles funciones para manejar la visibilidad de distintos elementos */
+// // Obtén los elementos
+// var figura = document.getElementById("figura");
+// var mensaje = document.getElementById("mensaje");
+// var textarea = document.querySelector("textarea");
+
+// // Función para encriptar
+// function encriptar() {
+//     figura.style.display = "none";
+//     mensaje.style.display = "none";
+// }
+
+// // Función para verificar el textarea
+// function verificarTextarea() {
+//     if (textarea.value === "") {
+//         figura.style.display = "block";
+//         mensaje.style.display = "block";
+//     }
+// }
+
+// // Agrega el evento input al textarea
+// textarea.addEventListener("input", verificarTextarea);
+
