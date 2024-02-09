ccec5575f1930cae021954ae3da473757ce892c8
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-02-08 23:56:43 -0300

    Avance en funciones
    Se continuó creando variables y funciones iniciales para capturar valores de los elementos HTML

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+let figura = document.getElementById("figura");
+let mensaje = document.getElementById("mensaje");
+let textoEntrante = document.getElementById("texto-entrada");
+let textoEncriptado = document.getElementById("texto-encriptado");
+let valorTextoEntrante = "";
+
+// Captura de contenido del texto entrante //
+textoEntrante.addEventListener("input", function() {
+    valorTextoEntrante = textoEntrante.value;
+})
+
 function encriptar() {
-    document.getElementById("figura").style.display = "none";
-    document.getElementById("mensaje").style.display = "none";
-    document.getElementById("texto-salida").style.display = "block";
+    figura.style.display = "none";
+    mensaje.style.display = "none";
+    textoEncriptado.style.display = "block";
+    textoEncriptado.value = valorTextoEntrante + "test";
 }
 
+function desencriptar() {
+    console.log("función de desencriptación");
+}
 
 
 /* Posibles funciones para manejar la visibilidad de distintos elementos */
