3089bb1c13ae2888975dea3ae20b6166c4afc4b2
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-02-09 18:06:25 -0300

    Nuevos en funciones
    Función de encriptación finalizada, pequeños cambios visuales y botón de copiar listo

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 let figura = document.getElementById("figura");
 let mensaje = document.getElementById("mensaje");
-let textoEntrante = document.getElementById("texto-entrada");
-let textoEncriptado = document.getElementById("texto-encriptado");
+let tituloMensaje = document.getElementById("titulo-mensaje");
+let infoMensaje = document.getElementById("info-mensaje");
+let botonCopiar = document.getElementById("boton-copiar");
+
+let textoEntrante = document.getElementById("texto-entrante");
 let valorTextoEntrante = "";
+let textoSaliente = document.getElementById("texto-saliente");
 
 // Captura de contenido del texto entrante //
-textoEntrante.addEventListener("input", function() {
-    valorTextoEntrante = textoEntrante.value;
-})
+textoEntrante.addEventListener("input", function () {
+  valorTextoEntrante = textoEntrante.value;
+});
 
+/* Claves de encriptación */
+
+let claves = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+// Función de encriptación //
 function encriptar() {
+  // Verifica si el texto es apto para encriptar.
+  // Si el texto contiene mayúsculas o caracteres especiales, no lo encripta.
+    let regExp = /^[a-z ]+$/g;
+        if (!regExp.test(valorTextoEntrante)) {
+            tituloMensaje.textContent = "Mensaje no aceptado";
+            infoMensaje.textContent ="Recuerda que solo se aceptan letras minúsculas y sin acentos";
+        return;
+  }
+
+  // Si pasa la verificación, oculta y muestra los elementos del área.
     figura.style.display = "none";
     mensaje.style.display = "none";
-    textoEncriptado.style.display = "block";
-    textoEncriptado.value = valorTextoEntrante + "test";
+    textoSaliente.style.display = "block";
+    botonCopiar.style.display = "flex";
+
+
+  // Encriptación
+    let textoCifrado = valorTextoEntrante.replace(/[aeiou]/g, function(vocal) {
+    return claves[vocal];
+  });
+
+  textoSaliente.value = textoCifrado;
 }
+
+// Función de desencriptado //
 
 function desencriptar() {
-    console.log("función de desencriptación");
+  console.log("función de desencriptación");
 }
 
+// Función de copiar //
+function copiar() {
+
+  navigator.clipboard.writeText(textoSaliente.value);
+}
 
 /* Posibles funciones para manejar la visibilidad de distintos elementos */
 // // Obtén los elementos
@@ -43,4 +83,3 @@ function desencriptar() {
 
 // // Agrega el evento input al textarea
 // textarea.addEventListener("input", verificarTextarea);
-
