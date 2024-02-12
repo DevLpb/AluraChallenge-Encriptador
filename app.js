955b8d663ddaa3f77004c89fbfeeaa70a6b99638
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-02-12 17:12:28 -0300

    Funciones finalizadas
    Fueron creadas las funciones para reiniciar los contenidos al borrar el texto por completo, y la función de desencriptar texto.

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ let textoEntrante = document.getElementById("texto-entrante");
 let valorTextoEntrante = "";
 let textoSaliente = document.getElementById("texto-saliente");
 
-// Captura de contenido del texto entrante //
+// Captura el contenido del texto entrante //
 textoEntrante.addEventListener("input", function () {
   valorTextoEntrante = textoEntrante.value;
 });
+
+function condicionesIniciales() {
+  figura.style.display = "block";
+  mensaje.style.display = "block";
+  tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+  infoMensaje.textContent = "Ingresa el texto que desees encriptar o desencriptar.";
+  textoSaliente.style.display = "none";
+  botonCopiar.style.display = "none";
+}
+
+// Función para verificar el textarea
+ function verificarEntrada() {
+    if (textoEntrante.value === "") {
+        condicionesIniciales();
+     }
+ }
+// Captura el contenido del texto entrante (comprueba si está vacío) //
+textoEntrante.addEventListener("input", verificarEntrada);
+
 
 /* Claves de encriptación */
 
@@ -42,6 +61,7 @@ function encriptar() {
 
 
   // Encriptación
+  // Busca las coincidencias de la expresión regular y las reemplaza por los valores de las propiedades del objeto "claves"
     let textoCifrado = valorTextoEntrante.replace(/[aeiou]/g, function(vocal) {
     return claves[vocal];
   });
@@ -49,37 +69,40 @@ function encriptar() {
   textoSaliente.value = textoCifrado;
 }
 
+
 // Función de desencriptado //
-
 function desencriptar() {
-  console.log("función de desencriptación");
+  // Verifica si el texto es apto para encriptar.
+  // Si el texto contiene mayúsculas o caracteres especiales, no lo encripta.
+    let regExp = /^[a-z ]+$/g;
+        if (!regExp.test(valorTextoEntrante)) {
+            tituloMensaje.textContent = "Mensaje no aceptado";
+            infoMensaje.textContent ="Recuerda que solo se aceptan letras minúsculas y sin acentos";
+        return;
 }
 
-// Función de copiar //
-function copiar() {
+    // Si pasa la verificación, oculta y muestra los elementos del área.
+    figura.style.display = "none";
+    mensaje.style.display = "none";
+    textoSaliente.style.display = "block";
+    botonCopiar.style.display = "flex";
 
-  navigator.clipboard.writeText(textoSaliente.value);
-}
 
-/* Posibles funciones para manejar la visibilidad de distintos elementos */
-// // Obtén los elementos
-// var figura = document.getElementById("figura");
-// var mensaje = document.getElementById("mensaje");
-// var textarea = document.querySelector("textarea");
+    // Desencriptado
+    // Busca las coincidencias de la expresión regular y las reemplaza, compara los valores de las propiedades del objeto "claves" y si son iguales, las reemplaza por el nombre de la propiedad.
+    let textoCifrado = valorTextoEntrante.replace(/(ai|enter|imes|ober|ufat)/g, function(clave) {
+      for (let vocal in claves) {
+        if (claves[vocal] === clave) {
+          return vocal;
+        }
+      }
+    });
 
-// // Función para encriptar
-// function encriptar() {
-//     figura.style.display = "none";
-//     mensaje.style.display = "none";
-// }
+    textoSaliente.value = textoCifrado;
+    }
 
-// // Función para verificar el textarea
-// function verificarTextarea() {
-//     if (textarea.value === "") {
-//         figura.style.display = "block";
-//         mensaje.style.display = "block";
-//     }
-// }
+    // Función de copiar //
+    function copiar() {
 
-// // Agrega el evento input al textarea
-// textarea.addEventListener("input", verificarTextarea);
+      navigator.clipboard.writeText(textoSaliente.value);
+    }
